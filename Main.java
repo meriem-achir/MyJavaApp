24c85f0cb9544270efdaac863442f5c8dfb9a16b
meriem-achir <meriem.achir20@gmail.com>
2024-11-05 12:28:25 +0100

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        int result = calculator.sum(10, 25);
+        System.out.println("La somme de 10 et 25 est : " + result);
+    }
+}
